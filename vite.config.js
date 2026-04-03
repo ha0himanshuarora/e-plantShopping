ff9134826dd845edd0d5011dcd8972421e13c199
ha0himanshuarora <ha0himanshuarora@gmail.com>
2026-04-03 20:59:57 +0530

    Fixing vite path issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: process.env.NODE_ENV === 'production' && !process.env.VERCEL ? "/shoppingreact" : "/",
   plugins: [react()],
 })
